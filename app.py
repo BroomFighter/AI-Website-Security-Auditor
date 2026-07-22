4ea1e03a5d51d19f5108dbacf00f4ae8626081e2
Anuk Bandulage <anuk.bandulage06@gmail.com>
2026-07-23 01:41:19 +0530

    debugging issues

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/scan", methods=["POST"])
+def scan():
+    website_url = request.form["url"]
+
+    return f"""
+    <h1>Website Scan Started</h1>
+    <p>Target Website:</p>
+    <p>{website_url}</p>
+    <br>
+    <a href="/">Go Back</a>
+    """
+
 
 if __name__ == "__main__":
     app.run(debug=True)
